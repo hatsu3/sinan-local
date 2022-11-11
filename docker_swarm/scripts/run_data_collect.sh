6e8d10a5b7e0f32c022459caf97c87800dfd5aca
yicheng.jin <jinyicheng4@gmail.com>
2022-11-11 11:37:23 -0500

    chore: modify data collecting script

--- docker_swarm/scripts/run_data_collect.sh
+++ docker_swarm/scripts/run_data_collect.sh
@@ -1,6 +1,6 @@
 cd ../
-python3 master_data_collect_ath_social.py --user-name yz2297 \
+python3 master_data_collect_ath_social.py --user-name yicheng \
 	--stack-name sinan-socialnet \
-	--min-users 4 --max-users 48 --users-step 2 \
-	--exp-time 1200 --measure-interval 1 --slave-port 40011 --deploy-config swarm_ath.json \
+	--min-users 2 --max-users 24 --users-step 2 \
+	--exp-time 1200 --measure-interval 1 --slave-port 40011 --deploy-config test_cluster.json \
 	--mab-config social_mab.json --deploy
--- docker_swarm/scripts/run_data_collect_hotel.sh
+++ docker_swarm/scripts/run_data_collect_hotel.sh
@@ -1,6 +1,6 @@
 cd ../
-python3 master_data_collect_ath_hotel.py --user-name yz2297 \
+python3 master_data_collect_ath_hotel.py --user-name yicheng \
 	--stack-name hotelreservation \
-	--min-users 2 --max-users 48 --users-step 1 \
-	--exp-time 750 --measure-interval 1 --slave-port 40011 --deploy-config hotel_swarm_ath.json \
+	--min-users 1 --max-users 24 --users-step 1 \
+	--exp-time 750 --measure-interval 1 --slave-port 40011 --deploy-config test_cluster_hotel.json \
 	--mab-config hotel_mab.json --deploy
